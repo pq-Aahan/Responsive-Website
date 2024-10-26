de32ff221f123ef7328b843f6fec62335f0a2427
pq-Aahan <pulast.aahan@gmail.com>
2024-10-26 23:27:16 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./Components/Button";
+import Header from "./Components/Header";
+
 const App = () => {
   return (
     <>
-      <h1 className="text-2xl font-bold underline">Hello test test world!</h1>;
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+      </div>
+      <ButtonGradient />
     </>
   );
 };
